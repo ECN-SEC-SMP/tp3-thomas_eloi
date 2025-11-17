89af2a3b502a7be93660c6f97b9d00bdc3f8d8fb
Thomas Verron <verronthomas03@gmail.com>
2025-11-17 16:50:01 +0100

    Implement rectangle.hpp

--- include/rectangle.hpp
+++ include/rectangle.hpp
@@ -4,7 +4,17 @@
 template <typename T>
 class Rectangle : public Forme<T> { 
     protected :
+    T longueur_;
+    T largeur_;    
+    public :  
+    Rectangle(Point<T> const &p1, T longueur, T largeur) : Forme<T>(p1), longueur_(longueur), largeur_(largeur) {}
+    ~Rectangle() = default;
+    T perimetre() override {
+        return 2 * (longueur_ + largeur_);
+    }
+    T surface() override {
+        return longueur_ * largeur_;
+    }
 
-    public :
-        
-}
+
+};
