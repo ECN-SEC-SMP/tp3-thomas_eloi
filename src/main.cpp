09fb9749e1ee487327b5687ed6253e4c67002fbe
Eloi Tourangin <etourangin@evolis.com>
2025-11-17 15:38:02 +0100

    initial commit.

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+/**
+ * @file main.cpp
+ * @author Eloi Tourangin (eloi.tourangin@eleves.ec-nantes.fr) & Thomas Verron (thomas.verron@eleves.ec-nantes.fr)
+ * @brief
+ * @version 0.1
+ * @date 17-11-2025
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
