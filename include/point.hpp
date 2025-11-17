3d80a220b2c9377ee26019bceaa2bc281a9ce626
Eloi Tourangin <eloi.tourangin@eleves.ec-nantes.fr>
2025-11-17 16:02:56 +0100

    implement methods of point.hpp

--- include/point.hpp
+++ include/point.hpp
@@ -1,19 +1,41 @@
+/**
+ * @file point.hpp
+ * @author Eloi Tourangin (eloi.tourangin@eleves.ec-nantes.fr) & Thomas Verron (thomas.verron@eleves.ec-nantes.fr)
+ * @brief
+ * @version 0.1
+ * @date 17-11-2025
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+
 #pragma once
+
 #include <iostream>
 
 using namespace std;
 
-template<typename T>
-class Point {
+template <typename T>
+class Point
+{
 protected:
     T x_;
     T y_;
+
 public:
-    Point(T x, T y);
-    Point(Point const& p);
-    void translater(T dx, t dy);
-    T getX();
-    T getY();
+    Point(T x, T y) : x_(x), y_(y) {}
+    Point(Point const &p) : x_(p.x_), y_(p.y_) {}
+    ~Point() = default;
+
+    void translater(T dx, T dy)
+    {
+        this->x_ += dx;
+        this->y_ += dy;
+    }
+
+    T getX() : return this->x_;
+    T getY() : return this->y_;
+    
     void changeX(T x) : x_(x) {}
     void changeY(T y) : y_(y) {}
 
